69fbc984338f81956f4317136502c3d52b3175b8
davrivas <d.rivas95@hotmail.com>
2017-11-11 20:55:00 -0500

    Inicial Fase III

---
+++ js/validar-venta.js
@@ -0,0 +1,13 @@
+$('document').ready(function() {
+    $('#vender').click(function() {
+        swal('Holaaaaaa');
+    });
+});
+/*function validarVenta() {
+    var placa = $('#placa').val(),
+        precio = $('#precio').val(),
+        marca = $('#precio').val(),
+        modelo = $('#modelo').val(),
+        referencia = $('referencia').val(),
+        cilindraje = $('')
+}*/
